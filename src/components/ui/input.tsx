843af77c5434210c6f719ad3b70633e2439c6e7f
Michel Freitas <michelfreitas2018@gmail.com>
2024-09-04 16:43:08 -0300

    feat: add icon to input element

--- src/components/ui/input.tsx
+++ src/components/ui/input.tsx
@@ -1,22 +1,35 @@
 import * as React from "react"
 
 import { cn } from "@/lib/utils"
+import { LucideIcon } from "lucide-react"
 
 export interface InputProps
-  extends React.InputHTMLAttributes<HTMLInputElement> {}
+  extends React.InputHTMLAttributes<HTMLInputElement> {
+  icon?: LucideIcon
+  iconStyle?: string
+}
 
 const Input = React.forwardRef<HTMLInputElement, InputProps>(
-  ({ className, type, ...props }, ref) => {
+  ({ icon, iconStyle, className, type, ...props }, ref) => {
+    const IconElement = icon;
     return (
-      <input
-        type={type}
-        className={cn(
-          "flex h-9 w-full rounded-md border border-input bg-transparent px-3 py-1 text-sm shadow-sm transition-colors file:border-0 file:bg-transparent file:text-sm file:font-medium placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:cursor-not-allowed disabled:opacity-50",
-          className
-        )}
-        ref={ref}
-        {...props}
-      />
+      <div className="relative">
+        <input
+          type={type}
+          className={cn(
+            "flex h-10 w-full rounded-md border border-input bg-background px-3 py-2 text-sm ring-offset-background file:border-0 file:bg-transparent file:text-sm file:font-medium placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50 transition-all duration-100",
+            className
+          )}
+          ref={ref}
+          {...props}
+        />
+        <div className="absolute inset-y-0 right-0 flex items-center pr-3">
+          {IconElement && <IconElement className={cn(
+            "dark:text-white text-slate-800",
+            iconStyle
+          )} />}
+        </div>
+      </div>
     )
   }
 )
