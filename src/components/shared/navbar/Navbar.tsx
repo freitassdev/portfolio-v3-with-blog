908750acfb2b34a68527173d6f13a2e55ed60b01
freitassdev <chelmizinho@gmail.com>
2024-09-04 19:56:23 -0300

    feat: add mobile navbar

--- src/components/shared/navbar/Navbar.tsx
+++ src/components/shared/navbar/Navbar.tsx
@@ -1,28 +1,62 @@
+'use client';
+
 import Image from "next/image";
 import logo from '../../../../public/images/logo-resources/logo-light-192x192.png';
 import { Separator } from "@/components/ui/separator";
 import { Button } from "@/components/ui/button";
-import { Github, Linkedin, Search } from "lucide-react";
+import { Github, Linkedin, Search, Menu } from "lucide-react";
 import { Input } from "@/components/ui/input";
+import { useRef, useState, useEffect } from "react";
 export default function Navbar({ active }: { active: string }) {
+    const [isMobile, setIsMobile] = useState<boolean>(false);
+    const [submenuWidth, setSubmenuWidth] = useState<number>(0);
+    const navRef = useRef<HTMLDivElement>(null);
 
+    useEffect(() => {
+        if (navRef.current) {
+            setSubmenuWidth(navRef.current.getBoundingClientRect().width);
+        }
+        window.addEventListener("resize", () => {
+            if (navRef.current) {
+                setSubmenuWidth(navRef.current.getBoundingClientRect().width);
+            }
+        });
+    }, [navRef]);
     return (
-        <nav className="fixed inset-x-0 top-4 z-40 md:mx-auto flex h-[60px] items-center justify-between rounded-xl border border-border bg-card px-2 shadow-sm saturate-100 backdrop-blur-[4px] transition-all duration-200 max-sm:mx-3 sm:mx-3 md:w-[600px] lg:w-[800px] xl:w-[1000px] 2xl:w-[1300px] ">
-            <div className="flex flex-row gap-3 items-center">
-                <Image className="h-[40px] w-auto rounded-md" src={logo} alt="Logo" />
-                <a href="/" className="text-xl font-bold text-primary">michelfreitas</a>
-            </div>
-            <div className="flex flex-row items-center gap-4">
-                <div className="flex flex-row items-center gap-4"> {/* items desktop */}
-                    <a href="#" className="text-lg text-primary transition-all duration-200 hover:text-primary/80 hover:border-b">Início</a>
-                    <Separator className="h-[20px]" orientation="vertical" />
-                    <a href="#" className="text-lg text-primary transition-all duration-200 hover:text-primary/80 hover:border-b">Sobre Mim</a>
-                    <Separator className="h-[20px]" orientation="vertical" />
-                    <a href="#" className="text-lg text-primary transition-all duration-200 hover:text-primary/80 hover:border-b">Blog</a>
-                    <Input placeholder="Pesquisar artigo..." iconStyle="h-5 w-5" className="border-border/50" icon={Search}/>
+        <>
+
+            <nav className="fixed inset-x-0 top-4 z-40 md:mx-auto flex h-[60px] items-center justify-between rounded-xl border border-border bg-card px-2 shadow-sm saturate-100 backdrop-blur-[4px] transition-all duration-200 max-sm:mx-3 sm:mx-3 md:w-[600px] lg:w-[800px] xl:w-[1000px] 2xl:w-[1300px]" ref={navRef}>
+                <div className="flex flex-row gap-3 items-center">
+                    <Image className="h-[40px] w-auto rounded-md" src={logo} alt="Logo" />
+                    <a href="/" className="text-xl font-bold text-primary">michelfreitas</a>
+                </div>
+                <div className="flex flex-row items-center gap-4">
+                    <div className="flex flex-row items-center gap-4"> {/* items desktop */}
+                        <a href="#" className="hidden md:block text-lg text-primary transition-all duration-200 hover:text-primary/80 hover:border-b">Início</a>
+                        <Separator className="hidden md:block h-[20px]" orientation="vertical" />
+                        <a href="#" className="hidden md:block text-lg text-primary transition-all duration-200 hover:text-primary/80 hover:border-b">Sobre Mim</a>
+                        <Separator className="hidden md:block h-[20px]" orientation="vertical" />
+                        <a href="#" className="hidden md:block text-lg text-primary transition-all duration-200 hover:text-primary/80 hover:border-b">Blog</a>
+                        <Input placeholder="Pesquisar artigo..." iconStyle="h-5 w-5 max-md:hidden" className="max-md:hidden border-border/50" icon={Search} />
+                        <Menu className="block md:hidden cursor-pointer" onClick={() => setIsMobile((prev) => !prev)} />
+                    </div>
+                </div>
+            </nav>
+            <div className={`md:hidden left-3 absolute bg-white top-24 ${isMobile ? "flex h-auto" : "hidden h-0"} shadow-sm saturate-100 backdrop-blur-[4px] bg-background/80 border border-border rounded-lg p-3 flex-col gap-5`}
+                style={{
+                    width: submenuWidth
+                }}>
+                <div className="flex flex-col justify-start items-center w-full gap-4">
+                    <a href="#" className="hidden max-md:block text-lg text-primary transition-all duration-200 hover:text-primary/80 hover:border-b">Início</a>
+                    <Separator className="hidden max-md:block max-w-48" />
+                    <a href="#" className="hidden max-md:block text-lg text-primary transition-all duration-200 hover:text-primary/80 hover:border-b">Sobre Mim</a>
+                    <Separator className="hidden max-md:block max-w-48" />
+                    <a href="#" className="hidden max-md:block text-lg text-primary transition-all duration-200 hover:text-primary/80 hover:border-b">Blog</a>
+                </div>
+                <div className="w-full">
+                    <Input placeholder="Pesquisar artigo..." iconStyle="h-5 w-5 md:hidden" className="md:hidden border-border/50" icon={Search} />
                 </div>
             </div>
-
-        </nav>
+        </>
     );
 };
